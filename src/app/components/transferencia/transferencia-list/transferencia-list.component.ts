b12abe6bbd029dbb1d260c15f79e73c0aff9ac31
jrpbjr <jrpbjr@gmail.com>
2023-12-05 15:07:59 -0300

    adicionado componente transferencia-list

---
+++ src/app/components/transferencia/transferencia-list/transferencia-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transferencia-list',
+  templateUrl: './transferencia-list.component.html',
+  styleUrls: ['./transferencia-list.component.css']
+})
+export class TransferenciaListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
