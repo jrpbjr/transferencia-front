450abd76c221d9851c6e727a4228ce985abefea9
jrpbjr <jrpbjr@gmail.com>
2023-12-06 14:07:26 -0300

    ts table transferencia.

--- src/app/components/transferencia/transferencia-list/transferencia-list.component.ts
+++ src/app/components/transferencia/transferencia-list/transferencia-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {Transferencia} from "../../../models/transferencia";
+import {TransferenciaService} from "../../../services/transferencia.service";
 
 @Component({
   selector: 'app-transferencia-list',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransferenciaListComponent implements OnInit {
 
-  constructor() { }
+  ELEMENT_DATA: Transferencia[] = []
+
+  displayedColumns: string[] = ['id', 'contaOrigem', 'contaDestino', 'valorTransferencia','dataTransferencia','taxa','acoes'];
+  dataSource = new MatTableDataSource<Transferencia>(this.ELEMENT_DATA);
+
+
+  constructor(
+      private service: TransferenciaService
+  ) { }
 
   ngOnInit(): void {
+
   }
 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+
+
 }
+
